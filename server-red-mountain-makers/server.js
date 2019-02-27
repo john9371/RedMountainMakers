221cf374b8acea5fb063ff15de0064690a3ea928
John McDaniel <john9371@aol.com>
2019-02-27 16:53:06 -0600

    post server compile

--- server-red-mountain-makers/server.js
+++ server-red-mountain-makers/server.js
@@ -58,5 +58,75 @@ router.get("/paymentHistory/:member_id", function (req, res) {
         });
     }
 });
+//Create User
+router.post("/users/:name:password:email:status:lastpaydate", function (req, res) {
+    con.query("INSERT INTO Member (name) VALUES (?, ?, ?, ?, ?, ?)", [req.params.name, req.params.email, req.params.password, req.params.status, false, req.params.lastpaydate], (err, rows, fields) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.send(rows);
+        }
+    });
+});
 
+//Read User
+router.get("/users/:id", function (req, res){
+    con.query("SELECT * FROM Member WHERE (id) = ?", [req.params.id], (err, rows, fields)=>{
+        if (err){
+            console.log(err);
+        }
+        else{
+            res.send(rows);
+        }
+    })
+});
+
+//Login User
+router.post("/users/:username:password", function(req, res){
+    con.query("SELECT * FROM Member WHERE (password) = ? AND (username) = ?", [req.params.password, req.params.username], (err, rows, fields)=>{
+        if (err){
+            console.log(err);
+        }
+        else{
+            res.send(rows);
+        }  
+    })
+});
+
+//Change User Password
+router.post("/users/:username:password", function(req, res){
+    con.query("UPDATE Member SET (password) = ?  WHERE (username) = ?", [req.params.password, req.params.username], (err, rows, fields)=>{
+        if (err){
+            console.log(err);
+        }
+        else{
+            res.send(rows);
+        }  
+    })
+});
+
+//Add Status
+router.post("/status/:id:name:price", function(req, res){
+    con.query("INSERT INTO Member_Status (id, membership_type, cost) VALUES  (?, ?, ?)", [req.params.id, req.params.name, req.params.price], (err, rows, fields)=>{
+        if (err){
+            console.log(err);
+        }
+        else{
+            res.send(rows);
+        } 
+    })
+});
+
+//Edit Status
+router.post("/status/:id:price", function(req, res){
+    con.query("UPDATE Member_Status SET (cost) VALUES  (?)", [req.params.id], (err, rows, fields)=>{
+        if (err){
+            console.log(err);
+        }
+        else{
+            res.send(rows);
+        } 
+    })
+});
 module.exports = router;
