f814febede73abea82c10e43ea6a4405cd1b47cd
Student <student@Admins-iMac.local>
2019-03-04 13:32:07 -0600

    update

--- red-mountain-makers/src/App.js
+++ red-mountain-makers/src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
-import Navbar from './Navbar'
-import Home from './Home.js'
+import Navbar from './Components/Navbar'
+import Home from './Components/Home.js'
+import Footer from './Components/footer'
 
-import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
+//import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
 import Login from './Components/Login'
 import './App.css';
 
 class App extends Component {
   render() {
-    return (<>
-    <Login />
-      <div className="App">
+    return (
+      <>
+        {/* <Login /> */}
         <Navbar />
         <Home />
-      </div>
+        <Footer />
       </>
     );
   }
--- red-mountain-makers/src/Components/footer.js
+++ red-mountain-makers/src/Components/footer.js
@@ -1,1 +1,29 @@
 import React, { Component } from 'react'
+
+
+function Footer() {
+    return (
+        <footer class="page-footer #d50000 red accent-4">
+            <div class="container">
+                <div class="row">
+                    <div class="col l6 s12">
+                        <h5 class="black-text text-darken-2">More Stuff</h5>
+                        <p class="black-text text-darken-2">blah blah blah</p>
+                    </div>
+
+                    <div class="col l3 s12">
+                        <h5 class="black-text text-darken-2">Connect</h5>
+                        <ul>
+                            <li><a class="black-text text-darken-2" href="#!">Link 1</a></li>
+                            <li><a class="black-text text-darken-2" href="#!">Link 2</a></li>
+                            <li><a class="black-text text-darken-2" href="#!">Link 3</a></li>
+                            <li><a class="black-text text-darken-2" href="#!">Link 4</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
