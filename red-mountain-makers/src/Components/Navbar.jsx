3ac273f32651bfea4047ec1b93a25ffb4f4741ae
Student <student@Admins-iMac.local>
2019-03-07 17:41:38 -0600

    front update

--- red-mountain-makers/src/Components/Navbar.jsx
+++ red-mountain-makers/src/Components/Navbar.jsx
@@ -8,8 +8,8 @@ function Navbar() {
             <div>
                 <div className="navbar-fixed">
                     <nav className="black darken-2 z-depth-4" role="navigation">
-                        <div className="nav-wrapper container"><a id="logo-container" href="http://localhost:3000/" className="brand-logo #d50000 red-text"><img src={Logo} style={{height:60}}></img></a>
-                            <ul className="right hide-on-med-and-down">
+                        <div className="nav-wrapper container"><a id="logo-container" href="http://localhost:3000/" className="brand-logo #d50000 red-text left"><img src={Logo} style={{height:60}}></img></a>
+                            <ul className="right ">
                                 <li><a className="#d50000 red-text" href="http://localhost:3000/">Home</a></li>
                                 <li><a className="#d50000 red-text" href="http://localhost:3000/Classes">Classes We Offer</a></li>
                                 <li><a className="#d50000 red-text" href="#">Membership</a></li>
@@ -17,14 +17,14 @@ function Navbar() {
                                 <li><a className="#d50000 red-text" href="http://localhost:3000/contact">Contact</a></li>
                             </ul>
                         </div>
-                        <ul id="nav-mobile" className="sidenav">
+                        {/* <ul id="nav-mobile" className="sidenav">
                             <li><a className="#d50000 red-text" href="http://localhost:3000/">Home</a></li>
                             <li><a className="#d50000 red-text" href="http://localhost:3000/Classes">Classes We Offer</a></li>
                             <li><a className="#d50000 red-text" href="#">Membership</a></li>
                             <li><a className="#d50000 red-text" href="#">Donate</a></li>
                             <li><a className="#d50000 red-text" href="http://localhost:3000/contact">Contact</a></li>
                         </ul>
-                        <a href="#" data-target="nav-mobile" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+                        <a href="#" data-target="nav-mobile" className="sidenav-trigger"><i className="material-icons">menu</i></a> */}
                     </nav>
                 </div>    
             </div>
--- red-mountain-makers/src/Components/footer.js
+++ red-mountain-makers/src/Components/footer.js
@@ -1,23 +1,23 @@
 import React from 'react'
-
+import Image from '../assets/RMMLogo.png'
 
 function Footer() {
     return (
-        <footer class="page-footer #d50000 red accent-4">
-            <div class="container">
-                <div class="row">
-                    <div class="col l6 s12">
-                        <h5 class="black-text text-darken-2">More Stuff</h5>
-                        <p class="black-text text-darken-2">blah blah blah</p>
+        <footer className="page-footer black darken-2">
+            <div className="container">
+                <div className="row">
+                    <div className="col l6 s12">
+                        <img src={Image} style={{height:100}}/>
+                        <p className="#d50000 red-text">Creativity Powered</p>
                     </div>
 
-                    <div class="col l3 s12">
-                        <h5 class="black-text text-darken-2">Connect</h5>
+                    <div className="col l3 s12">
+                        <h5 className="#d50000 red-text">Connect</h5>
                         <ul>
-                            <li><a class="black-text text-darken-2" href="#!">Link 1</a></li>
-                            <li><a class="black-text text-darken-2" href="#!">Link 2</a></li>
-                            <li><a class="black-text text-darken-2" href="#!">Link 3</a></li>
-                            <li><a class="black-text text-darken-2" href="#!">Link 4</a></li>
+                            <li><a className="#d50000 red-text" href="#!">Link 1</a></li>
+                            <li><a className="#d50000 red-text" href="#!">Link 2</a></li>
+                            <li><a className="#d50000 red-text" href="#!">Link 3</a></li>
+                            <li><a className="#d50000 red-text" href="#!">Link 4</a></li>
                         </ul>
                     </div>
                 </div>
