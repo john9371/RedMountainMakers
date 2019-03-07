6ad39cb66fe314af4fb2e2da6a0eb453e4fbf835
JamesM23 <jhmay30@gmail.com>
2019-03-07 10:39:25 -0600

    contact

--- red-mountain-makers/src/App.js
+++ red-mountain-makers/src/App.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import Navbar from './Components/Navbar'
 import Home from './Components/Home'
 import Footer from './Components/footer'
-import Classes from './Components/Classes'
+//import Classes from './Components/Classes'
 // import Membership from './'
 import Contact from './Components/Contact'
-import Admin from './Components/Admin'
+//import Admin from './Components/Admin'
 // import Supporters from './'
 import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
 //import Login from './Components/Login'
@@ -21,11 +21,11 @@ class App extends Component {
           <Navbar />
             <Switch>
               <Route exact path="/" component={Home}/>
-              <Route exact path="/Classes" component={Classes}/>
+              {/* <Route exact path="/Classes" component={Classes}/> */}
               {/* <Route exact path="/Membership" component={Membership}/> */}
               <Route exact path="/Contact" component={Contact}/>
               {/* <Route exact path="/Supporters" component={Supporters}/> */}
-              <Route exact path="/Admin" component={Admin}/>
+              {/* <Route exact path="/Admin" component={Admin}/> */}
             </Switch>
           <Footer />  
         </>
