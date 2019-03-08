78310c81aecb0d640c3cbd8fb59c6c05cc748045
KentMG <4gunns.k@gmail.com>
2019-03-08 11:09:14 -0600

    Updating Login

--- server-red-mountain-makers/server.js
+++ server-red-mountain-makers/server.js
@@ -13,3 +13,6 @@ app.get('/', function (req, res) {
 app.listen(process.env.PORT || 5000, function () {
   console.log('Node app is running on port 5000');
 });
+
+
+10:06:48	CREATE TABLE Member (     member_id INT NOT NULL,     name varchar(50)  NOT NULL,     email varchar(50) not null,      password varchar(150) not null,     date_created datetime DEFAULT CURRENT_TIMESTAMP,     member_status int not null,     liability_waiver boolean not null,     last_payed date not null,     parent_id int,     PRIMARY KEY (id) )	Error Code: 1067. Invalid default value for 'date_created'	0.022 sec
