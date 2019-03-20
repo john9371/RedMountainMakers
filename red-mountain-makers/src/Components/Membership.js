f1b0b04abad2fa324b311ede3de2989db6cccea0
John McDaniel <john9371@aol.com>
2019-03-20 16:10:13 -0500

    stuff

--- red-mountain-makers/src/Components/Membership.js
+++ red-mountain-makers/src/Components/Membership.js
@@ -14,17 +14,18 @@ export default class Membership extends Component {
                     <p class="flow-text">
                         <div class="center-align"><p><h1>How to become a Maker</h1><br />
                             Come Tour the space! We have open ours Tuesday, Thursday, and Sunday 6:30pm - 9:00pm<br />
-                            Become a Basic Member, its super easy! Purchase your membership here! Since an important<br />
-                            part of the maker space philosophy is trust, we want to get a sense of who you are.You will<br />
-                            also want to evaluate our community to see if it's a good fit for you as well. This is why all<br />
-                            members start out as basic members.Once you've been out for a few meetings, you can apply to<br />
+                            
+                            Become a Basic Member, its super easy! Purchase your membership here! Since an important
+                            part of the maker space philosophy is trust, we want to get a sense of who you are.You will
+                            also want to evaluate our community to see if it's a good fit for you as well. This is why all
+                            members start out as basic members.Once you've been out for a few meetings, you can apply to
                             become a Full Maker Membership.<br />
                             <h2>Full Members must also fulfill the following responsibilities:</h2>
-                            A member must thoughtfully contribute to Red Mountain Makers’s direction and<br />
-                            policies.A member must pay the monthly fee as determined by the board of<br />
-                            directors.At the time a member's eligibility expires, he must forfeit his or her<br />
-                            method of entry in addition to any other property owned by Red Mountain Makers<br />
-                            to a member of the board of directors or an agent designated by the board of<br />
+                            A member must thoughtfully contribute to Red Mountain Makers’s direction and
+                            policies.A member must pay the monthly fee as determined by the board of
+                            directors.At the time a member's eligibility expires, he must forfeit his or her
+                            method of entry in addition to any other property owned by Red Mountain Makers
+                            to a member of the board of directors or an agent designated by the board of
                             directors for this purpose.
                         </p></div></p>
                     <div className="line">
