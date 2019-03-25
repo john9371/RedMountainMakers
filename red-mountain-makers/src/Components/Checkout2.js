f7e4463b85cb43f1caa36a9725e533e6fe3ef807
Lithelli <leepatner@ymail.com>
2019-03-25 09:41:24 -0500

    payment

--- red-mountain-makers/src/Components/Checkout2.js
+++ red-mountain-makers/src/Components/Checkout2.js
@@ -6,12 +6,9 @@ class App extends Component {
   render() {
     return (
       <StripeProvider apiKey="pk_test_aEnBl38XEUIxKbay6USa7dvK">
-        <div className="example">
-          <h1>React Stripe Elements Example</h1>
           <Elements>
             <CheckoutForm />
           </Elements>
-        </div>
       </StripeProvider>
     );
   }
--- red-mountain-makers/src/Components/Payment.js
+++ red-mountain-makers/src/Components/Payment.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import {CardElement, injectStripe} from 'react-stripe-elements';
+import {Button, Row, Col} from 'react-materialize';
+import '../css/PayMe.css'
 
 export default class Payment extends Component{
     constructor(props) {
         super(props);
-        this.state = { complete: false };
+        this.state ={
+             complete: false,
+             NAME: '',
+             EMAIL: '',
+         };
         this.submit = this.submit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
   
-  
+    handleChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+      }
+
     async submit(ev) {
         let {token} = await this.props.stripe.createToken({name: "Name"});
         let response = await fetch("/charge", {
@@ -25,9 +37,29 @@ export default class Payment extends Component{
 
         return (
             <div className="checkout">
-                <p>Would you like to complete the purchase?</p>
-                <CardElement />
-                <button onClick={this.submit}>Send</button>
+            <h2>Would you like to complete the purchase?</h2>
+            <form >
+                <input placeholder="Name" name="NAME" type="text" value={this.state.name} onChange={this.handleChange}/>
+                <input placeholder="Email" name="EMAIL" type="text" value={this.state.email} onChange={this.handleChange}/>
+                <CardElement className="PayMe" style={{
+                    base: {
+                    iconColor: '#666EE8',
+                    color: '#31325F',
+                    lineHeight: '40px',
+                    fontWeight: 300,
+                    fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+                    fontSize: '12px',
+                    '::placeholder': {
+                        color: '#CFD7E0',
+                    }
+                }
+            }} />
+            <Row>
+                <Col s={12} m={12} l={12} style={{display:'flex', justifyContent:'center'}}>
+                 <Button onClick={this.submit} className="payMeButton">Send</Button>
+                </Col>
+            </Row>
+            </form>
             </div>
         );
     }
